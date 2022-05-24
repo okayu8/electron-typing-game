3eac7b993e69f0cb5146501b3206f855a53d66e4
okayu8 <hitugaya12@gmail.com>
2022-05-24 22:49:03 +0900

    styling page

--- src/renderer/view/pages/Score/index.tsx
+++ src/renderer/view/pages/Score/index.tsx
@@ -1,6 +1,7 @@
 import { TPageList } from '../../../../common/types';
 import { PAGE_LIST } from '../../../../common/const';
 import { TScore } from '../../../../common/types';
+import { Button } from '../../components/Button';
 
 export type TScorePage = {
   setCurrentPage: (pageName: TPageList) => void;
@@ -9,14 +10,14 @@ export type TScorePage = {
 
 export const ScorePage = ({ setCurrentPage, score }: TScorePage) => {
   return (
-    <div>
+    <>
       <h1>Score</h1>
       <p>time: {score.displayTime}</p>
       <p>number of all typed words: {score.numberOfAllTyped}</p>
       <p>miss type: {score.missType}</p>
-      <button onClick={() => setCurrentPage(PAGE_LIST.START)}>
+      <Button onClick={() => setCurrentPage(PAGE_LIST.START)}>
         Go to start page
-      </button>
-    </div>
+      </Button>
+    </>
   );
 };
--- src/renderer/view/pages/Typing/index.tsx
+++ src/renderer/view/pages/Typing/index.tsx
@@ -1,6 +1,10 @@
+import styled from '@emotion/styled';
+
 import { TextToType } from '../../components/TextToType';
 import { typingContainer } from './typingContainer';
+import { PAGE_LIST } from '../../../../common/const';
 import { TPageList, TScore } from '../../../../common/types';
+import { Button } from '../../components/Button';
 
 type TTypingPage = {
   setCurrentPage: (pageName: TPageList) => void;
@@ -18,17 +22,54 @@ export const TypingPage = ({
   } = typingContainer({ setCurrentPage, setScore, numberOfQuestions });
 
   return (
-    <div>
-      <div>
-        {word && (
-          <TextToType
-            text={word}
-            onClearDisplayWord={handleClearDisplayWord}
-            countUpMissTypes={handleMissTypes}
-          />
-        )}
-      </div>
-      <div>{playTime}</div>
-    </div>
+    <Wrapper>
+      <Header>
+        <Button onClick={() => setCurrentPage(PAGE_LIST.START)}>BACK</Button>
+        <TimeWrapper>
+          <p>TIME:</p>
+          <Time>{playTime}</Time>
+        </TimeWrapper>
+      </Header>
+      <TextWrapper>
+        <TextToType
+          text={word}
+          onClearDisplayWord={handleClearDisplayWord}
+          countUpMissTypes={handleMissTypes}
+        />
+      </TextWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  margin: auto;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+`;
+
+const TimeWrapper = styled.div`
+  display: flex;
+  justify-content: right;
+  font-weight: bold;
+
+  > p {
+    margin: 0;
+  }
+`;
+
+const Time = styled.p`
+  width: 60px;
+  display: flex;
+  justify-content: right;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+`;
